93033d08ca12ee3f4db528d955d6be6ff183adad
Muhammed Saheer <mshaheerk6@gmail.com>
2025-10-02 16:56:02 +0530

    Updates

---
+++ precache-manifest.57e41f5879b3026847cd856ffdb72c5b.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609fc21dcef58202d8d360a6c86f7f88",
+    "url": "/hava---Vinyl-Store/index.html"
+  },
+  {
+    "revision": "8c6d829e893c45d71c18",
+    "url": "/hava---Vinyl-Store/static/css/2.7446b4c7.chunk.css"
+  },
+  {
+    "revision": "44cfb127fc928498d6c2",
+    "url": "/hava---Vinyl-Store/static/css/main.83a4ab22.chunk.css"
+  },
+  {
+    "revision": "8c6d829e893c45d71c18",
+    "url": "/hava---Vinyl-Store/static/js/2.933da522.chunk.js"
+  },
+  {
+    "revision": "cf6acb57bea48eeed438",
+    "url": "/hava---Vinyl-Store/static/js/3.8ae09a66.chunk.js"
+  },
+  {
+    "revision": "44cfb127fc928498d6c2",
+    "url": "/hava---Vinyl-Store/static/js/main.367755b6.chunk.js"
+  },
+  {
+    "revision": "174250652b95e46d9583",
+    "url": "/hava---Vinyl-Store/static/js/runtime~main.726d35ec.js"
+  },
+  {
+    "revision": "90a1699918bc72cc0970b73ba731fa07",
+    "url": "/hava---Vinyl-Store/static/media/black-logo.90a16999.png"
+  },
+  {
+    "revision": "087fae4422a39c2f252842d7570cd476",
+    "url": "/hava---Vinyl-Store/static/media/header-disk.087fae44.svg"
+  },
+  {
+    "revision": "b231d8085d979f04e742e10fe927576c",
+    "url": "/hava---Vinyl-Store/static/media/hero-image2.b231d808.jpg"
+  },
+  {
+    "revision": "8245638be60396a4c6ea515392c6028b",
+    "url": "/hava---Vinyl-Store/static/media/hero-image3.8245638b.svg"
+  },
+  {
+    "revision": "e7750dcf2980a1cafa68530effb4d48e",
+    "url": "/hava---Vinyl-Store/static/media/product1-hover.e7750dcf.webp"
+  },
+  {
+    "revision": "290090f9a1579b4e7572610f8edec624",
+    "url": "/hava---Vinyl-Store/static/media/product1.290090f9.webp"
+  },
+  {
+    "revision": "0bbac807c4127330b2517bf568450a87",
+    "url": "/hava---Vinyl-Store/static/media/product2-hover.0bbac807.webp"
+  },
+  {
+    "revision": "3eb9f60487d9ac27e7bf719b8930587d",
+    "url": "/hava---Vinyl-Store/static/media/product2.3eb9f604.webp"
+  },
+  {
+    "revision": "07529c80ca1601804532615b2e0b1ee9",
+    "url": "/hava---Vinyl-Store/static/media/product3-hover.07529c80.avif"
+  },
+  {
+    "revision": "4b61278d57566bf6bc38b7f09442677b",
+    "url": "/hava---Vinyl-Store/static/media/product3.4b61278d.webp"
+  },
+  {
+    "revision": "0d6069c1a3f334e6be08c5bbe4aa6a18",
+    "url": "/hava---Vinyl-Store/static/media/product4-hover.0d6069c1.webp"
+  },
+  {
+    "revision": "e95cedeb35cb4c571a379a35dab2fc8d",
+    "url": "/hava---Vinyl-Store/static/media/product4.e95cedeb.webp"
+  },
+  {
+    "revision": "195041e8ca67bb226404e838185681f9",
+    "url": "/hava---Vinyl-Store/static/media/product5-hover.195041e8.webp"
+  },
+  {
+    "revision": "b7a43aaace1cca5d0f36773309de2e20",
+    "url": "/hava---Vinyl-Store/static/media/product5.b7a43aaa.webp"
+  },
+  {
+    "revision": "a91478e4eb1c122d8a708681f5944488",
+    "url": "/hava---Vinyl-Store/static/media/product6-hover.a91478e4.webp"
+  },
+  {
+    "revision": "4e6cb50ac83e3e9f7b19fa62998aa364",
+    "url": "/hava---Vinyl-Store/static/media/product6.4e6cb50a.avif"
+  },
+  {
+    "revision": "2e625ef1bc4a6eb73b6a40d50a440914",
+    "url": "/hava---Vinyl-Store/static/media/product7-hover.2e625ef1.avif"
+  },
+  {
+    "revision": "0af192f21d7fbd43920ac91dde972b2f",
+    "url": "/hava---Vinyl-Store/static/media/product7.0af192f2.webp"
+  },
+  {
+    "revision": "76938ca796dfd5521c94ee02f5dcfe98",
+    "url": "/hava---Vinyl-Store/static/media/product8-hover.76938ca7.webp"
+  },
+  {
+    "revision": "56b175efb633d2b37a12978eb70faa26",
+    "url": "/hava---Vinyl-Store/static/media/product8.56b175ef.webp"
+  },
+  {
+    "revision": "b61d78b274faf94b02de136cadc89551",
+    "url": "/hava---Vinyl-Store/static/media/retro-vinyl-scene.b61d78b2.jpg"
+  },
+  {
+    "revision": "7fff4e9ffa086f9afdb6e7fddf555c2c",
+    "url": "/hava---Vinyl-Store/static/media/slider1.7fff4e9f.jpg"
+  },
+  {
+    "revision": "399363d264c639f8d273a49da8238887",
+    "url": "/hava---Vinyl-Store/static/media/slider10.399363d2.jpg"
+  },
+  {
+    "revision": "04b37db3f76eec6c1c147d53082f4492",
+    "url": "/hava---Vinyl-Store/static/media/slider2.04b37db3.jpg"
+  },
+  {
+    "revision": "6081e006d1b2abf6ab2b8851d5152a16",
+    "url": "/hava---Vinyl-Store/static/media/slider3.6081e006.jpg"
+  },
+  {
+    "revision": "96e0426f49fbb9fdd085a4bca034649a",
+    "url": "/hava---Vinyl-Store/static/media/slider4.96e0426f.jpg"
+  },
+  {
+    "revision": "516c548856e5d8fb5978b00474ac3920",
+    "url": "/hava---Vinyl-Store/static/media/slider5.516c5488.jpg"
+  },
+  {
+    "revision": "48c20108cc9f10eb97ccdc63893fafae",
+    "url": "/hava---Vinyl-Store/static/media/slider6.48c20108.jpg"
+  },
+  {
+    "revision": "43ebc8c5b46279e2a5d99efeb2c53d8f",
+    "url": "/hava---Vinyl-Store/static/media/slider7.43ebc8c5.jpg"
+  },
+  {
+    "revision": "3f7b947f04fc79a616d67cac54aad586",
+    "url": "/hava---Vinyl-Store/static/media/slider8.3f7b947f.jpg"
+  },
+  {
+    "revision": "79cce81a1f79a10634808bbeb287cc58",
+    "url": "/hava---Vinyl-Store/static/media/slider9.79cce81a.jpg"
+  },
+  {
+    "revision": "fec05cfe0221ea701c68c240ae504fc4",
+    "url": "/hava---Vinyl-Store/static/media/white-logo.fec05cfe.png"
+  }
+]);
